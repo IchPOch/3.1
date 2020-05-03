f9dfc4f435e3a109823a2bc25509e83336f291d1
Миша Епринцев <meprincev@bk.ru>
2020-05-04 01:05:28 +0300

    123

---
+++ 3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _3._1
+{
+    class Program
+    {
+        public void f(int x)
+        {
+            int a, b, c;
+            a = x / 100;
+            b = (x / 10) % 10;
+            c = x % 10;
+            Console.WriteLine(c + "" + b + "" + a);
+        }
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Введите трёхзнач число ");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x < 100 || x > 999) Console.WriteLine("Неккоректные введённые данные");
+            else
+            {
+                Program m = new Program();
+                m.f(x);
+            }
+        }
+    }
+}
